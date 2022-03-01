039cef89304ca1018da82affe6028713304ec7ef
PratyushSoni <pratyushsoni.bt21ele@pec.edu.in>
2022-03-01 20:31:50 +0530

    Assignment 4

---
+++ Assignment 4/21104043_Assgn4_Output.py
@@ -0,0 +1,83 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:\Users\HP\python\Assgn4\abc.py ==================
+Question 1
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+
+
+
+Question 2
+Enter the number of rows you want: 10
+                                  1                                     
+                                1   1                                   
+                              1   2   1                                 
+                            1   3   3   1                               
+                          1   4   6   4   1                             
+                       1   5   10   10   5   1                          
+                    1   6   15   20   15   6   1                        
+                  1   7   21   35   35   21   7   1                     
+               1   8   28   56   70   56   28   8   1                   
+            1   9   36   84   126   126   84   36   9   1               
+
+	Iterative Procedure
+
+Enter the number of rows you want: 8
+                        1                           
+                      1   1                         
+                    1   2   1                       
+                  1   3   3   1                     
+                1   4   6   4   1                   
+             1   5   10   10   5   1                
+          1   6   15   20   15   6   1              
+        1   7   21   35   35   21   7   1           
+--------------------------------------------------------------------------------
+Question 3
+Enter 1st number: 5352
+Enter 2nd number: 24
+The quotient is 223 and the remainder is 0
+False
+False
+True
+False
+[223, 0, 4, 5, 6]
+[0, 4]
+{0, 4}
+frozenset({0, 4})
+4
+4
+
+
+
+Question 4
+Student's name is Akash
+Roll number is 3080
+
+
+
+Question 5
+| Employee   <==>   Salary   |
+|   Mehak    <==>    40000   |
+|   Ashok    <==>    50000   |
+|   Viren    <==>    60000   |
+
+The salary of Mehak is updated to 70000
+
+Viren's Record is deleted
+
+
+
+Question 6
+Enter George's word: arc
+Enter Barbie's word: car
+Their friendship is True.
+
+
+
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-END OF ASSIGNMENT-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
